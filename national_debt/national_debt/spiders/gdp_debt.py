b54f246a15728436a60d581ba5128b9e621fe935
soreb <44830196+soreb@users.noreply.github.com>
2021-01-03 17:02:01 +0100

    Add files via upload

---
+++ national_debt/national_debt/spiders/gdp_debt.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GdpDebtSpider(scrapy.Spider):
+    name = 'gdp_debt'
+    allowed_domains = ['worldpopulationreview.com']
+    start_urls = ['http://worldpopulationreview.com/countries/countries-by-national-debt/']
+
+    def parse(self, response):
+        rows = response.xpath("//table/tbody/tr")
+        for row in rows:
+            yield {
+                'country_name': row.xpath(".//td[1]/a/text()").get(),
+                'gdp_debt': row.xpath(".//td[2]/text()").get()
+            }
