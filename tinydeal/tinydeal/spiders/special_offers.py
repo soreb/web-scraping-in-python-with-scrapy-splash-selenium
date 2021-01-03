5673aa158e9ebec5579edb6f958090f4b2589d31
soreb <44830196+soreb@users.noreply.github.com>
2021-01-03 17:13:25 +0100

    Add files via upload

---
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SpecialOffersSpider(scrapy.Spider):
+    name = 'special_offers'
+    allowed_domains = ['www.cigabuy.com']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.cigabuy.com/consumer-electronics-c-56_75-pg-1.html', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+        })
+
+    def parse(self, response):
+        for product in response.xpath("//div[@class='p_box_wrapper']"):
+            yield {
+                'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                'discoutned_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
+                'original_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(),
+                'User-Agent': response.request.headers['User-Agent']
+            }
+
+        next_page = response.xpath("//a[@class='nextPage']/@href").get()
+
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+            })
